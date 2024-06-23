ca7d698fa5394fae8a4479683334657e23279a41
Sakiyama Shingo <yamaa.exception@gmail.com>
2024-06-23 22:29:39 +0900

    feature: Gameコンポーネントのインタラクションテストを追加

--- src/app/(main)/_components/Game.stories.ts
+++ src/app/(main)/_components/Game.stories.ts
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import { fn } from '@storybook/test';
+import { expect, fn, userEvent, within } from '@storybook/test';
 import { Game } from './Game';
 
 const meta = {
@@ -10,6 +10,35 @@ const meta = {
     layout: 'fullscreen',
   },
   args: {},
+  play: async ({ canvasElement,step }) => {
+    const canvas = within(canvasElement);
+
+    const buttons =  canvas.getAllByRole('button');
+    const filteredButtons = buttons.filter((button) => button.textContent === '');
+    await step('左上', async () => {
+      await userEvent.click(filteredButtons[0]);
+    });
+    await step('上真ん中', async () => {
+      await userEvent.click(filteredButtons[1]);
+    });
+    await step('右上', async () => {
+      await userEvent.click(filteredButtons[2]);
+    });
+    await step('左真ん中', async () => {
+      await userEvent.click(filteredButtons[3]);
+    });
+    await step('真ん中', async () => {
+      await userEvent.click(filteredButtons[4]);
+    });
+    await step('右真ん中', async () => {
+      await userEvent.click(filteredButtons[5]);
+    });
+    await step('左下', async () => {
+      await userEvent.click(filteredButtons[6]);
+    });
+
+   expect(canvas.getByText('Winner: X')).toBeInTheDocument();
+  },
 } satisfies Meta<typeof Game>;
 
 export default meta;
